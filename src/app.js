7bc99db7e853df82d27cefd8391fe1f6c51bda13
Alro127 <nhuttan373@gmail.com>
2025-08-15 09:43:30 +0700

    nt

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const sequelize = require('./config/database');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Test kết nối DB
 sequelize.authenticate()
   .then(() => console.log('Database connected!'))
   .catch(err => console.error('Unable to connect to DB:', err));
+
+const ticketsRoute = require('./routes/ticketsRoute');
+app.use('/api/tickets', ticketsRoute);
 
 module.exports = app;
